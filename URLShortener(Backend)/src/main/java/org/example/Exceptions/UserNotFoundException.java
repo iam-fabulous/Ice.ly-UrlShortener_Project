a3762035cca55313de1dc2e71040fa9dda0e3d1e
iam-fabulous <emmanuelebukainnah@gmail.com>
2025-05-15 15:20:10 +0100

    changed the filename

--- URLShortener(Backend)/src/main/java/org/example/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.Exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
